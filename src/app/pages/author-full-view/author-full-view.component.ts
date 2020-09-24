92d51f76e0022a35bbb2e0eee34bc5b6b46cbfee
Maksim Kirilko <maksimkirilko@gmail.com>
2020-09-24 21:42:44 +0300

    Add architect info

--- src/app/pages/author-full-view/author-full-view.component.ts
+++ src/app/pages/author-full-view/author-full-view.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+
+import { IAppStore } from '../../redux/state.model';
+import { AuthorsService } from '../../services/authors.service';
+import { AuthorModel } from '../../models/author.model';
 
 @Component({
   selector: 'app-author-full-view',
@@ -6,10 +12,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./author-full-view.component.scss']
 })
 export class AuthorFullViewComponent implements OnInit {
+  architect: AuthorModel;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private store: Store<IAppStore>, private authorsServide: AuthorsService) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(({ id }) => {
+      this.authorsServide.getAuthor(id).subscribe((archtect: AuthorModel) => {
+        console.log(archtect);
+        this.architect = archtect;
+      });
+    });
   }
 
 }
