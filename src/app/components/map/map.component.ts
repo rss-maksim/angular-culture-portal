40bc04c75f767ef1e431c0623de34fd378770ffb
haid_roman <haidroman@gmail.com>
2020-09-25 23:18:33 +0300

    map fixes

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AgmMarker } from '@agm/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { WorksModel } from 'src/app/models/works.model';
 
 @Component({
@@ -6,21 +7,39 @@ import { WorksModel } from 'src/app/models/works.model';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnChanges {
 
   @Input() sights: WorksModel[];
 
   public sightsOnMap: WorksModel[];
 
-  public isMapShown = false;
+  public map: google.maps.Map;
 
+  public isMapShown = false;
+  public latitude: number;
+  public longitude: number;
   constructor() { }
 
-  ngOnInit(): void {
+  public ngOnChanges(): void {
     this.sightsOnMap = this.sights.filter((sight) => {
       return sight.lat ? true : false;
     });
-    this.isMapShown = this.sightsOnMap.length > 0;
+    if (this.sightsOnMap.length > 0) {
+      this.isMapShown = true;
+      this.latitude = this.sightsOnMap[0].lat;
+      this.longitude = this.sightsOnMap[0].lng;
+    }
   }
 
+  public onMapReady(map: google.maps.Map): void {
+    this.map = map;
+  }
+
+  public onMarkerClick(marker: AgmMarker): void {
+    this.map.setCenter({
+      lat: marker.latitude,
+      lng: marker.longitude
+    });
+    this.map.setZoom(12);
+  }
 }
