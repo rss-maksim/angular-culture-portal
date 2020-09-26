896296435c19986e70e6d310e3cbf995753b6fde
Maksim Kirilko <maksimkirilko@gmail.com>
2020-09-26 15:57:35 +0300

    Fix architects source

--- src/app/const.ts
+++ src/app/const.ts
@@ -1,7 +1,7 @@
 export const LOCALE_KEY = 'LOCALE';
 
 export const AUTHORS_FILE = 'https://raw.githubusercontent.com/rss-maksim/angular-culture-portal' +
-  '/develop/src/app/data/architects/';
+  '/develop/src/assets/architects/';
 export const AUTHORS_EXTENSION = '.json';
 
 export const navItems = [
